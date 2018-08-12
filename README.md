c691b3f9bee0a841eed6344207b28e24e5dd062a
Mohamed Fizal Ihsan Mohamed <fizalihsan@gmail.com>
2018-08-12 00:29:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Commands
+
+* To start the services: `mvn lagom:runAll`
+* GET: `curl http://localhost:9000/api/hello/World`
+
+# Technology Stack
+
+* Lagom Framework
---
+++ domain-impl/src/test/java/com/fizal/domain/impl/HelloEntityTest.java
@@ -0,0 +1,54 @@
+package com.fizal.domain.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.lightbend.lagom.javadsl.testkit.PersistentEntityTestDriver;
+import com.lightbend.lagom.javadsl.testkit.PersistentEntityTestDriver.Outcome;
+
+import akka.Done;
+import akka.actor.ActorSystem;
+import akka.testkit.JavaTestKit;
+import com.fizal.domain.impl.HelloEvent.GreetingMessageChanged;
+
+public class HelloEntityTest {
+
+  static ActorSystem system;
+
+  @BeforeClass
+  public static void setup() {
+    system = ActorSystem.create("HelloEntityTest");
+  }
+
+  @AfterClass
+  public static void teardown() {
+    JavaTestKit.shutdownActorSystem(system);
+    system = null;
+  }
+
+  @Test
+  public void testHelloWorld() {
+    PersistentEntityTestDriver<HelloCommand, HelloEvent, HelloState> driver = new PersistentEntityTestDriver<>(system,
+        new HelloEntity(), "world-1");
+
+    Outcome<HelloEvent, HelloState> outcome1 = driver.run(new HelloCommand.Hello("Alice"));
+    assertEquals("Hello, Alice!", outcome1.getReplies().get(0));
+    assertEquals(Collections.emptyList(), outcome1.issues());
+
+    Outcome<HelloEvent, HelloState> outcome2 = driver.run(new HelloCommand.UseGreetingMessage("Hi"),
+        new HelloCommand.Hello("Bob"));
+    assertEquals(1, outcome2.events().size());
+    assertEquals(new GreetingMessageChanged("world-1", "Hi"), outcome2.events().get(0));
+    assertEquals("Hi", outcome2.state().message);
+    assertEquals(Done.getInstance(), outcome2.getReplies().get(0));
+    assertEquals("Hi, Bob!", outcome2.getReplies().get(1));
+    assertEquals(2, outcome2.getReplies().size());
+    assertEquals(Collections.emptyList(), outcome2.issues());
+  }
+
+}
